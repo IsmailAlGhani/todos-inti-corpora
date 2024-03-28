a85b0f83dfce6eb891cd2ba152156ecfc1a2c128
Ismail Al Ghani <ismailalghani@gmail.com>
2024-03-28 19:27:02 +0700

    init project

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+/// <reference types="vitest" />
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: ["./vitest.setup.ts"],
+    coverage: {
+      provider: "v8",
+    },
+  },
+});
